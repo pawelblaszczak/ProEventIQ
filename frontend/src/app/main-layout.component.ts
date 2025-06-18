f6789f6729e5122f63b333bba6f1b64fad69bfe5
Pawel Blaszczak <pawel.blaszczak@comarch.pl>
2025-06-18 15:28:06 +0200

    frontend - layout

--- frontend/src/app/main-layout.component.ts
+++ frontend/src/app/main-layout.component.ts
@@ -2,13 +2,26 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from './material.module';
+import { MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipDefaultOptions } from '@angular/material/tooltip';
+
+// Custom tooltip behavior
+export const myTooltipDefaults: MatTooltipDefaultOptions = {
+  showDelay: 300,
+  hideDelay: 100,
+  touchendHideDelay: 100,
+  touchGestures: 'auto',
+  position: 'right'
+};
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [CommonModule, RouterModule, MaterialModule],
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
+  providers: [
+    { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myTooltipDefaults }
+  ]
 })
 export class MainLayoutComponent {
   isExpanded = signal<boolean>(true);
